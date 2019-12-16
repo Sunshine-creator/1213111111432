3b8265da22c5a740f767f05eba1870d0ed33c927
Sunshine <3538545066@163.com>
2019-12-16 22:37:09 +0800

    C语言代码

---
+++ dagar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	printf("输入两个操作数:>");
+	scanf("%d %d", &a, &b);
+	
+	sum = num1 + num2;
+	printf(" sum=%d\n", sum);
+	system("pause");
+
+	return 0;
+}
